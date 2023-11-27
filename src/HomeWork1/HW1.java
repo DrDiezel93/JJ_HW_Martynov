56544efe28d8854a42987e216506a72bc0ba3869
Dr.Ddiezel93 <ghek93@mail.ru>
2023-11-27 23:50:48 +0300

    Correct HW1

--- src/HomeWork1/HW1.java
+++ src/HomeWork1/HW1.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class HW1 {
     public static void main(String[] args) {
-        List<Integer> lst = Arrays.asList(1, 5, 9, 3, 2, 5, 7, 6);
+        List<Integer> lst = Arrays.asList(1, 2, 8, 4, 2, 5, 7, 6);
 //        List<Integer> lst2 = new ArrayList<>();
         double averageLst = getAverage(lst);
         System.out.println(averageLst);
@@ -14,6 +14,7 @@ public class HW1 {
 
     private static double getAverage(List<Integer> list) {
         return list.stream()
+                .filter(a -> a % 2 == 0)
                 .mapToInt(a -> a)
                 .average().orElse(0); // возвращает ноль, если список пуст
     }
